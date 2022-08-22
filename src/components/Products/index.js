0361389181b107f3cfa50adbe194c5b3af4d450c
Dwidy99 <98dwiyulianto@gmail.com>
2022-08-22 09:34:06 +0700

    link product to detail product

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,5 +1,6 @@
 import styles from "./Products.module.css";
 import { Container, Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import Cart from "../../assets/images/icons/cart-48.png";
 import Product1 from "../../assets/images/product/Cannon-EF.jpg";
@@ -18,11 +19,13 @@ const ContentProduct = () => {
         <Row className="d-flex justify-content-center">
           <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
             <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product1}
-              />
+              <Link to="/detail">
+                <Card.Img
+                  className={styles.imgProduct}
+                  variant="top"
+                  src={Product1}
+                />
+              </Link>
               <Card.Body>
                 <Row>
                   <Col className="col col-md-12">
@@ -42,11 +45,13 @@ const ContentProduct = () => {
           </Col>
           <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
             <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product3}
-              />
+              <Link to="/detail">
+                <Card.Img
+                  className={styles.imgProduct}
+                  variant="top"
+                  src={Product2}
+                />
+              </Link>
               <Card.Body>
                 <Row>
                   <Col className="col col-md-12">
@@ -66,11 +71,13 @@ const ContentProduct = () => {
           </Col>
           <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
             <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product2}
-              />
+              <Link to="/detail">
+                <Card.Img
+                  className={styles.imgProduct}
+                  variant="top"
+                  src={Product3}
+                />
+              </Link>
               <Card.Body>
                 <Row>
                   <Col className="col col-md-12">
@@ -90,11 +97,13 @@ const ContentProduct = () => {
           </Col>
           <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
             <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product3}
-              />
+              <Link to="/detail">
+                <Card.Img
+                  className={styles.imgProduct}
+                  variant="top"
+                  src={Product1}
+                />
+              </Link>
               <Card.Body>
                 <Row>
                   <Col className="col col-md-12">
@@ -114,11 +123,13 @@ const ContentProduct = () => {
           </Col>
           <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
             <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product1}
-              />
+              <Link to="/detail">
+                <Card.Img
+                  className={styles.imgProduct}
+                  variant="top"
+                  src={Product2}
+                />
+              </Link>
               <Card.Body>
                 <Row>
                   <Col className="col col-md-12">
@@ -138,11 +149,13 @@ const ContentProduct = () => {
           </Col>
           <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
             <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product3}
-              />
+              <Link to="/detail">
+                <Card.Img
+                  className={styles.imgProduct}
+                  variant="top"
+                  src={Product3}
+                />
+              </Link>
               <Card.Body>
                 <Row>
                   <Col className="col col-md-12">
@@ -162,11 +175,13 @@ const ContentProduct = () => {
           </Col>
           <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
             <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product2}
-              />
+              <Link to="/detail">
+                <Card.Img
+                  className={styles.imgProduct}
+                  variant="top"
+                  src={Product3}
+                />
+              </Link>
               <Card.Body>
                 <Row>
                   <Col className="col col-md-12">
@@ -186,11 +201,13 @@ const ContentProduct = () => {
           </Col>
           <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
             <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product3}
-              />
+              <Link to="/detail">
+                <Card.Img
+                  className={styles.imgProduct}
+                  variant="top"
+                  src={Product1}
+                />
+              </Link>
               <Card.Body>
                 <Row>
                   <Col className="col col-md-12">
@@ -210,107 +227,13 @@ const ContentProduct = () => {
           </Col>
           <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
             <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product1}
-              />
-              <Card.Body>
-                <Row>
-                  <Col className="col col-md-12">
-                    <Card.Title>Cannon Camera</Card.Title>
-                  </Col>
-                </Row>
-                <Row className={styles.desc}>
-                  <Col>
-                    <Card.Text>Rp. 8.500.000,-</Card.Text>
-                  </Col>
-                  <Col className="d-flex justify-content-end">
-                    <img src={Cart} alt="cart" width="30" height="30" />
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
-            <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product3}
-              />
-              <Card.Body>
-                <Row>
-                  <Col className="col col-md-12">
-                    <Card.Title>Cannon Camera</Card.Title>
-                  </Col>
-                </Row>
-                <Row className={styles.desc}>
-                  <Col>
-                    <Card.Text>Rp. 8.500.000,-</Card.Text>
-                  </Col>
-                  <Col className="d-flex justify-content-end">
-                    <img src={Cart} alt="cart" width="30" height="30" />
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
-            <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product2}
-              />
-              <Card.Body>
-                <Row>
-                  <Col className="col col-md-12">
-                    <Card.Title>Cannon Camera</Card.Title>
-                  </Col>
-                </Row>
-                <Row className={styles.desc}>
-                  <Col>
-                    <Card.Text>Rp. 8.500.000,-</Card.Text>
-                  </Col>
-                  <Col className="d-flex justify-content-end">
-                    <img src={Cart} alt="cart" width="30" height="30" />
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
-            <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product1}
-              />
-              <Card.Body>
-                <Row>
-                  <Col className="col col-md-12">
-                    <Card.Title>Cannon Camera</Card.Title>
-                  </Col>
-                </Row>
-                <Row className={styles.desc}>
-                  <Col>
-                    <Card.Text>Rp. 8.500.000,-</Card.Text>
-                  </Col>
-                  <Col className="d-flex justify-content-end">
-                    <img src={Cart} alt="cart" width="30" height="30" />
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="col col col-lg-3 col-md-4 col-sm-5 col-6 mb-4 mt-4">
-            <Card className={styles.card}>
-              <Card.Img
-                className={styles.imgProduct}
-                variant="top"
-                src={Product2}
-              />
+              <Link to="/detail">
+                <Card.Img
+                  className={styles.imgProduct}
+                  variant="top"
+                  src={Product1}
+                />
+              </Link>
               <Card.Body>
                 <Row>
                   <Col className="col col-md-12">
