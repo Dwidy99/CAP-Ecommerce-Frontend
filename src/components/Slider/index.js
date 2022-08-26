828fd459fb92e43248d55eb7b16918596e8cffdb
Dwidy99 <98dwiyulianto@gmail.com>
2022-08-26 15:20:40 +0700

    improve slider

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -2,7 +2,7 @@ import Slide1 from "../../assets/images/banner2.jpg";
 import Slide2 from "../../assets/images/banner1.jpg";
 import Slide3 from "../../assets/images/banner3.jpg";
 import React, { useState } from "react";
-import { Carousel, Container } from "react-bootstrap";
+import { Carousel, Col, Container, Row } from "react-bootstrap";
 
 const Slider = () => {
   const [index, setIndex] = useState(0);
@@ -12,18 +12,22 @@ const Slider = () => {
   };
 
   return (
-    <Container className="d-flex justify-content-center">
-      <Carousel activeIndex={index} onSelect={handleSelect} variant="dark">
-        <Carousel.Item>
-          <img className="d-block w-85" src={Slide1} alt="First slide" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-85" src={Slide3} alt="Second slide" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-85" src={Slide2} alt="Third slide" />
-        </Carousel.Item>
-      </Carousel>
+    <Container fluid>
+      <Row>
+        <Col>
+          <Carousel activeIndex={index} onSelect={handleSelect} variant="dark">
+            <Carousel.Item>
+              <img className="d-block w-100" src={Slide1} alt="First slide" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={Slide3} alt="Second slide" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={Slide2} alt="Third slide" />
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
     </Container>
   );
 };
